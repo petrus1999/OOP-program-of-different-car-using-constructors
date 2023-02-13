fe0579b6c1f5715d81a31b149445157656a55fb8
petrus <pshai671@gmail.com>
2023-02-13 09:26:27 +0200

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_program_of_different_car_using_constructors
+{
+     class Car
+    {
+        string brand;
+        int price;
+
+        // parameterized constructor
+        Car(string theBrand, int thePrice)
+        {
+
+            brand = theBrand;
+            price = thePrice;
+        }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using OOP_program_of_different_car_using_constructors;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        // call parameterized constructor
+        Car car1 = new Car("Bugatti", 50000);
+
+        Console.WriteLine("Brand: " + car1.brand);
+        Console.WriteLine("Price: " + car1.price);
+        Console.ReadLine();
+
+    }
+}
